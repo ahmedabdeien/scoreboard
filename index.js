e9e3058b470f3fb0f9fb2e93face90d14e94eeb8
Ahmed Abdeen <ahmedabdeien@gmail.com>
2024-10-30 16:06:11 +0300

    change  Welcome to my website!

--- index.js
+++ index.js
@@ -15,36 +15,36 @@
 // })
 
 
-let number1 = 5
-let number2 = 7
+// let number1 = 5
+// let number2 = 7
 
-let num1 = document.getElementById("num1-el") 
-let num2 = document.getElementById("num2-el")
-let sumEl = document.getElementById("sum-el")
+// let num1 = document.getElementById("num1-el") 
+// let num2 = document.getElementById("num2-el")
+// let sumEl = document.getElementById("sum-el")
 
-num1.textContent = number1
-num2.textContent = number2
-// Create four functions: add(), subtract(), divide(), multiply()
-// Call the correct function when the user clicks on one of the buttons
-// Perform the given calculation using num1 and num2
-// Render the result of the calculation in the paragraph with id="sum-el"
+// num1.textContent = number1
+// num2.textContent = number2
+// // Create four functions: add(), subtract(), divide(), multiply()
+// // Call the correct function when the user clicks on one of the buttons
+// // Perform the given calculation using num1 and num2
+// // Render the result of the calculation in the paragraph with id="sum-el"
 
-// E.g. if the user clicks on the "Plus" button, you should render
-// "Sum: 10" (since 8 + 2 = 10) inside the paragraph with id="sum-el"
-function add(){
+// // E.g. if the user clicks on the "Plus" button, you should render
+// // "Sum: 10" (since 8 + 2 = 10) inside the paragraph with id="sum-el"
+// function add(){
     
-    let result = number1 + number2
-    sumEl.textContent = "Sun: " + result
-}
-function subtract(){
-    let result = number1 - number2
-    sumEl.textContent = "Sun: " + result
-}
-function divide(){
-    let result = number1 / number2
-    sumEl.textContent = "Sun: " + result
-}
-function multiply(){
-    let result = number1 * number2
-    sumEl.textContent = "Sun: " + result
-}
+//     let result = number1 + number2
+//     sumEl.textContent = "Sun: " + result
+// }
+// function subtract(){
+//     let result = number1 - number2
+//     sumEl.textContent = "Sun: " + result
+// }
+// function divide(){
+//     let result = number1 / number2
+//     sumEl.textContent = "Sun: " + result
+// }
+// function multiply(){
+//     let result = number1 * number2
+//     sumEl.textContent = "Sun: " + result
+// }
