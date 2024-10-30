ad2542f8c5766cea833b4972a629054f3b510c30
Ahmed Abdeen <ahmedabdeien@gmail.com>
2024-10-30 16:01:35 +0300

    Commit initial files

---
+++ index.js
@@ -0,0 +1,50 @@
+// When the user clicks the purchase button, render out
+// "Something went wrong, please try again" in the paragraph
+// that has the id="error".
+
+
+// let error = document.getElementById("error")
+// let btn = document.getElementById("btn")
+
+
+// btn.addEventListener("click", function(){
+    
+//     error.textContent = "Something went wrong, please try again";
+//     error.style.color="#666666"
+//     error.style.backgroundColor="#f1f1f1"
+// })
+
+
+let number1 = 5
+let number2 = 7
+
+let num1 = document.getElementById("num1-el") 
+let num2 = document.getElementById("num2-el")
+let sumEl = document.getElementById("sum-el")
+
+num1.textContent = number1
+num2.textContent = number2
+// Create four functions: add(), subtract(), divide(), multiply()
+// Call the correct function when the user clicks on one of the buttons
+// Perform the given calculation using num1 and num2
+// Render the result of the calculation in the paragraph with id="sum-el"
+
+// E.g. if the user clicks on the "Plus" button, you should render
+// "Sum: 10" (since 8 + 2 = 10) inside the paragraph with id="sum-el"
+function add(){
+    
+    let result = number1 + number2
+    sumEl.textContent = "Sun: " + result
+}
+function subtract(){
+    let result = number1 - number2
+    sumEl.textContent = "Sun: " + result
+}
+function divide(){
+    let result = number1 / number2
+    sumEl.textContent = "Sun: " + result
+}
+function multiply(){
+    let result = number1 * number2
+    sumEl.textContent = "Sun: " + result
+}
